27b393f11e00e9b05dac75721ab5f75441d4b44b
Kiran <bandapally-sai.kiran@capgemini.com>
2022-11-30 16:09:28 +0530

    firstt committ

--- src/main/java/com/forexservice/Swagger/SwaggerConfig.java
+++ src/main/java/com/forexservice/Swagger/SwaggerConfig.java
@@ -2,24 +2,20 @@ package com.forexservice.Swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
-
 import java.util.List;
-
-
-import springfox.documentation.builders.ApiInfoBuilder;
-
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
@@ -45,7 +41,7 @@ public class SwaggerConfig {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Furniture - Spring Boot Swagger Configuration")
+                .title("Forex services - Spring Boot Swagger Configuration")
                 .description("\"Swagger configuration for application \"")
                 .version("1.1.0")
                 .license("Apache 2.0")
